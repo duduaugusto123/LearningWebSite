29246ee29082f293cef194da36a49b3f97e0551c
duduaugusto123 <eduardoaugustodossantos123@gmail.com>
2023-05-25 22:57:44 -0300

    DevLearning1.0

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+window.addEventListener('scroll', function() {
+    var conteudos = document.querySelectorAll('.conteudo');
+    
+    for (var i = 0; i < conteudos.length; i++) {
+      var conteudo = conteudos[i];
+      var posicaoTop = conteudo.getBoundingClientRect().top;
+      var posicaoJanela = window.innerHeight * 0.8; /* Define a posição da janela em que o elemento deve aparecer */
+      
+      if (posicaoTop < posicaoJanela) {
+        conteudo.classList.add('aparecer');
+      }
+    }
+  });
 
